122aca1efb11d0e9dae5f2c75046e7b3e7dfb054
xeox <xeox27@gmail.com>
2018-08-25 14:08:21 +0900

    enjoy3

---
+++ enjoy3/src/enjoy3/enjoy3.java
@@ -0,0 +1,11 @@
+package enjoy3;
+
+public class enjoy3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I think the laundry would dry well"
+				+ "because the weather is good today.");
+	}
+
+}
